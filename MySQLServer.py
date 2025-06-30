1f85911b91a401f90d7ad759e2fc652ce620962b
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-30 20:19:03 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,33 @@
-mysql.connector 
+import mysql.connector 
 from mysql.connector import errorcode
 
+def create_database():
+  connections = None
+  cursor = None
+  database = alx_book_store
+
+  try:
+    connections = mysql.connector.connect(
+      host="localhost",
+      user="root@localhot", 
+      password="Navalayo@1970"
+    )
+    connections.cursor()
+    
+    create_databse = f"CREATE DATABASE IF NOT EXISTS {database}"
+    cursor.execute(create_database)
+    print(f"Database {database} created successfully!")
+
+  except mysql.connector.Error as err:
+    print(f"an unexpected error occurred: {err}")
+  except Exception as e:
+    print(f"can't create a database due to unknown error {e]")
+  finaally :
+    if cursor:
+            cursor.close()
+        if cnx and cnx.is_connected():
+            cnx.close()
+            print("MySQL connection closed.")
+
+if __name__ == "__main__":
+  create_database()
